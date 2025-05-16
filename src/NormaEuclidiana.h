2d3d6ec9513718ffe27b392b89b0bd2657f345c3
Pedro <pedroghansel@gmail.com>
2025-05-16 01:59:09 -0300

    Implementação do calculo da norma euclidiana

--- src/NormaEuclidiana.h
+++ src/NormaEuclidiana.h
@@ -1,57 +1,28 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-
 #ifndef NORMAEUCLIDIANA_H_INCLUDED
 #define NORMAEUCLIDIANA_H_INCLUDED
 
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
 class NormaEuclidiana
 {
-private:
-    float *vet;
-    int dimensao;
 public:
-    NormaEuclidiana();
-    ~NormaEuclidiana();
-    void calcularNorma();
-    float calcularNorma_FuncaoInterna(float *vet, int dimensao);
+    double normaEuclidiana(vector<double>);
 };
 
-#endif // NORMAEUCLIDIANA_H_INCLUDED
-
-NormaEuclidiana::NormaEuclidiana(){}
-
-NormaEuclidiana::~NormaEuclidiana(){
-    delete[] vet;
-}
-
-void NormaEuclidiana::calcularNorma()
-{   
-    cout << "De o numero de dimensoes do vetor: "<< endl;
-    cin >> dimensao;
-    vet = new float[dimensao];
-    cout << endl<< "De os valores do vetor: "<< endl;
-    for (int i = 0; i < dimensao; i++)
-    {
-        cin >> vet[i];
-    }
-
-    float soma = 0;
-    for (int i = 0; i < dimensao; i++)
-    {
-        soma += vet[i] * vet[i];
-    }
-    float norma = sqrt(soma);
-
-    cout << endl << "Norma Euclidiana do vetor (magnitude): " << norma << endl;
-}
-
-float NormaEuclidiana::calcularNorma_FuncaoInterna(float *vet, int dimensao)
+double NormaEuclidiana::normaEuclidiana(vector<double> vet)
 {
-    float soma = 0;
-    for (int i = 0; i < dimensao; i++)
+    double sum = 0.0;
+    for(int i = 0; i < vet.size(); i++)
     {
-        soma += vet[i] * vet[i];
+        vet[i] = pow(vet[i], 2); 
+        sum += vet[i];   
     }
-    return sqrt(soma);
+    double result = sqrt(sum);
+    return result;
 }
+
+#endif // NORMAEUCLIDIANA_H_INCLUDED
